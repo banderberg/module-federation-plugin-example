1f792a1075aa1c443753ee6a5de8ca1c9099a889
banderberg <banderberg@gmail.com>
2025-07-25 13:54:55 -0500

    app

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  standalone: true,
-  selector: 'app-root',
-  imports: [
-    RouterModule
-  ],
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    imports: [
+        RouterModule
+    ],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'shell';
